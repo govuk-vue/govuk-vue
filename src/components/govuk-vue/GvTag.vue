7e223dc91a43a2f690e77bfdca0727fe1980e5b1
Matt Eason <github@matteason.co.uk>
2023-03-16 17:09:27 +0000

    Add tag component

---
+++ src/components/govuk-vue/GvTag.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import hasSlot from '@/composables/useHasSlot'
+
+defineProps({
+  text: String,
+  classes: {
+    type: String,
+    default: ''
+  },
+  colour: {
+    type: String,
+    validator(value: string) {
+      return [
+        'grey',
+        'green',
+        'turquoise',
+        'blue',
+        'purple',
+        'pink',
+        'red',
+        'orange',
+        'yellow'
+      ].includes(value)
+    }
+  }
+})
+</script>
+
+<template>
+  <strong :class="`govuk-tag ${classes} ${colour ? `govuk-tag--${colour}` : ''}`">
+    <template v-if="hasSlot('default')">
+      <slot />
+    </template>
+    <template v-else>
+      {{ text }}
+    </template>
+  </strong>
+</template>
+
+<style scoped lang="scss">
+@import 'node_modules/govuk-frontend/govuk/components/tag/tag';
+</style>
