d8e05d5ca8a047cc605defc82fe74da158986540
Matt Eason <github@matteason.co.uk>
2023-05-22 12:25:48 +0100

    Make SSR-safe

--- src/composables/useIsDesktop.ts
+++ src/composables/useIsDesktop.ts
@@ -1,15 +1,18 @@
 import { ref, onMounted, onUnmounted } from 'vue'
 
 export function useIsDesktop() {
-  // Set the matchMedia to the govuk-frontend desktop breakpoint
-  const mediaQuery = window.matchMedia('(min-width: 48.0625em)')
-  const isDesktop = ref(mediaQuery.matches)
+  let mediaQuery: MediaQueryList
+  const isDesktop = ref(false)
 
   function updateIsDesktop() {
     isDesktop.value = mediaQuery.matches
   }
 
   onMounted(() => {
+    // Set up mediaQuery in onMounted so that we know we're running on the client, not SSR
+    // min-width is the govuk-frontend desktop breakpoint
+    mediaQuery = window.matchMedia('(min-width: 48.0625em)')
+    updateIsDesktop()
     mediaQuery.addEventListener('change', updateIsDesktop)
   })
 
